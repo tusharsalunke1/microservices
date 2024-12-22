805286d960758f9896fce1695c942e5b55d67154
tusharsalunke1 <96986371+tusharsalunke1@users.noreply.github.com>
2024-12-22 11:58:09 +0530

    Delete currency-exchange-service directory

--- currency-exchange-service/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
-values(1001,'USD','INR',10,'8000');
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
-values(1002,'INR','USD',10,'8000');
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
-values(1003,'trr','geee',10,'8000');
